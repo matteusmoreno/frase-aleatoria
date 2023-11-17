da30ea0a3d106d15ab5761446ea9a0a6d2f6cb77
Matteus Moreno <matteus_moreno@live.com>
2023-11-17 13:19:08 -0300

    inicio do projeto

---
+++ src/main/java/br/com/alura/frasealeatoria/FraseAleatoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.frasealeatoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraseAleatoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraseAleatoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/frasealeatoria/FraseAleatoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.frasealeatoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraseAleatoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
