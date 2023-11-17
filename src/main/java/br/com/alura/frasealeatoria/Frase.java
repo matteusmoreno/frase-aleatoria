c24023b7f032a25cd2d53625238aa4ae4cb0944e
Matteus Moreno <matteus_moreno@live.com>
2023-11-17 13:21:35 -0300

    criando a entidade Frase

---
+++ src/main/java/br/com/alura/frasealeatoria/Frase.java
@@ -0,0 +1,21 @@
+package br.com.alura.frasealeatoria;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "frases")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Frase {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String personagem;
+    private String poster;
+}
